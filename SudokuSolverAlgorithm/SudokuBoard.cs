90719643ef5e25ed5a14ed41d769605dcfb88976
Marco Huber <huber.marco@freenet.de>
2023-08-06 20:01:58 +0200

    Add project files.

---
+++ SudokuSolverAlgorithm/SudokuBoard.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace SudokuSolverAlgorithm
+{
+    public class SudokuBoard : List<List<char>>
+    {
+        public SudokuBoard(char[][] board)
+        {
+            foreach (var row in board)
+                Add(row.ToList());
+        }
+
+        public bool AreThereNewPartialSolutionSteps()
+        {
+            var areThereNewPartialSolutionSteps = this.Any(row => row.Any(element => element == '.'));
+            return areThereNewPartialSolutionSteps;
+        }
+
+        public void SelectNewPartialSolutionStep()
+        {
+
+        }
+    }
+}
---
+++ SudokuSolverAlgorithmTests/SudokuSolverAlgorithmsTests.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using SudokuSolverAlgorithm;
+
+namespace SudokuSolverAlgorithmTests
+{
+    [TestFixture]
+    public class SudokuSolverAlgorithmsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void When_sudoku_solver_is_called_then_result_is_calculated()
+        {
+            
+            var board = new char[][]
+            {
+                new char[] {'5','3','.','.','7','.','.','.','.'},
+                new char[] {'6','.','.','1','9','5','.','.','.'},
+                new char[] {'.','9','8','.','.','.','.','6','.'},
+                new char[] {'8','.','.','.','6','.','.','.','3'},
+                new char[] {'4','.','.','8','.','3','.','.','1'},
+                new char[] {'7','.','.','.','2','.','.','.','6'},
+                new char[] {'.','6','.','.','.','.','2','8','.'},
+                new char[] {'.','.','.','4','1','9','.','.','5'},
+                new char[] {'.','.','.','.','8','.','.','7','9'}
+            };
+
+            var xx = board[7][5];
+
+            new SudokuSolver().SolveSudoku(board);
+        }
+    }
+}
