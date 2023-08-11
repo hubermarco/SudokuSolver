1841c6ec97d6d331669cfe489662bf524c8058f3
Marco Huber <huber.marco@freenet.de>
2023-08-11 16:15:56 +0200

    SudokuSolverAlgorithmsTests corrected

--- SudokuSolverAlgorithmTests/SudokuBoardTests.cs
+++ SudokuSolverAlgorithmTests/SudokuBoardTests.cs
@@ -17,7 +17,7 @@ namespace SudokuSolverAlgorithmTests
         }
 
         [Test]
-        public void When_there_are_new_partial_solution_steps_available_then_true_is_returned()
+        public void When_board_is_valid_then_true_is_returned()
         {
             var board = new char[][]
             {
@@ -34,7 +34,7 @@ namespace SudokuSolverAlgorithmTests
 
             var sudokuBoard = new SudokuBoard(board);
 
-            Assert.IsTrue(sudokuBoard.IsSolutionComplete());
+            Assert.IsTrue(sudokuBoard.IsBoardValid());
         }
 
         [Test]
@@ -42,20 +42,20 @@ namespace SudokuSolverAlgorithmTests
         {
             var board = new char[][]
             {
-                new char[] {'5','3','4','5','7','3','6','7','9'},
-                new char[] {'5','3','4','5','7','3','6','7','9'},
-                new char[] {'5','3','4','5','7','3','6','7','9'},
-                new char[] {'5','3','4','5','7','3','6','7','9'},
-                new char[] {'5','3','4','5','7','3','6','7','9'},
-                new char[] {'5','3','4','5','7','3','6','7','9'},
-                new char[] {'5','3','4','5','7','3','6','7','9'},
-                new char[] {'5','3','4','5','7','3','6','7','9'},
-                new char[] {'5','3','4','5','7','3','6','7','9'}
+               new char[] {'5','3','4','6','7','8','9','1','2'},
+               new char[] {'6','7','2','1','9','5','3','4','8'},
+               new char[] {'1','9','8','3','4','2','5','6','7'},
+               new char[] {'8','5','9','7','6','1','4','2','3'},
+               new char[] {'4','2','6','8','5','3','7','9','1'},
+               new char[] {'7','1','3','9','2','4','8','5','6'},
+               new char[] {'9','6','1','5','3','7','2','8','4'},
+               new char[] {'2','8','7','4','1','9','6','3','5'},
+               new char[] {'3','4','5','2','8','6','1','7','9'}
             };
 
             var sudokuBoard = new SudokuBoard(board);
 
-            Assert.IsFalse(sudokuBoard.IsSolutionComplete());
+            Assert.IsTrue(sudokuBoard.IsSolutionComplete());
         }
 
         [Test]
