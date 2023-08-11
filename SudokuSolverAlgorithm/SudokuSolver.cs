303aebf176ac0e6b6be98fd82d52f2203fb6840c
Marco Huber <huber.marco@freenet.de>
2023-08-11 09:10:38 +0200

    Little refactoring

--- SudokuSolverAlgorithm/SudokuSolver.cs
+++ SudokuSolverAlgorithm/SudokuSolver.cs
@@ -1,31 +1,4 @@
-﻿using System;
-
-/*
-boolean FindeLoesung(int index, Lsg loesung, ...)
-{
-    // index = Schrittzahl
-    // loesung = Referenz auf Teillösung
-    while (es gibt noch neue Teil-Lösungsschritte) {
-    Wähle einen neuen Teil-Lösungsschritt schritt; // Heuristik
-    if (schritt ist gültig) {
-        Erweitere loesung um schritt;
-        if (loesung noch nicht vollständig) {
-            // rekursiver Aufruf von FindeLoesung
-            if (FindeLoesung(index + 1, loesung, ...))
-            {
-                return true; // Lösung gefunden
-            }
-            else
-            { // wir sind in einer Sackgasse
-                Mache schritt rückgängig; // Backtracking
-            }
-        } else return true; // Lösung gefunden -> fertig
-    }
-}
-return false;
-} // Bei true als Rückgabewert steht die Lösung in loesung
-*/
-namespace SudokuSolverAlgorithm
+﻿namespace SudokuSolverAlgorithm
 {
     public class SudokuSolver
     {
@@ -63,16 +36,10 @@ namespace SudokuSolverAlgorithm
                 }
             }
             return false;
-            }
-
-        private static int CalculateNextNumber(int index)
-        {
-            return index % 9 + 1; ;
         }
 
-        private static bool AreThereNewPartialSolutionSteps(int numberSum)
-        {
-            return numberSum < 45;
-        }
+        private static int CalculateNextNumber(int index) => index % 9 + 1; 
+
+        private static bool AreThereNewPartialSolutionSteps(int numberSum) => numberSum < 45;
     }
 }
